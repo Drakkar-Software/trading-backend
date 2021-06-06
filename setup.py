56e13799915eb8606fb14da36e1229610fef3425
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2021-06-06 22:46:55 +0200

    projet initialization

---
+++ setup.py
@@ -0,0 +1,60 @@
+#  Drakkar-Software OctoBot-Tentacles-Manager-Launcher
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+# from distutils.extension import Extension
+from setuptools import find_packages
+from setuptools import setup
+
+from octobot_tentacles_manager import PROJECT_NAME, VERSION
+
+PACKAGES = find_packages(exclude=["tests"])
+
+packages_list = []
+
+# long description from README file
+with open('README.md', encoding='utf-8') as f:
+    DESCRIPTION = f.read()
+
+REQUIRED = open('requirements.txt').readlines()
+REQUIRES_PYTHON = '>=3.8'
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    url='https://github.com/Drakkar-Software/trading-backend',
+    license='LGPL-3.0',
+    author='Drakkar-Software',
+    author_email='drakkar-software@protonmail.com',
+    description='Trading tools',
+    packages=PACKAGES,
+    include_package_data=True,
+    long_description=DESCRIPTION,
+    tests_require=["pytest"],
+    test_suite="tests",
+    zip_safe=False,
+    data_files=[],
+    setup_requires=REQUIRED,
+    install_requires=REQUIRED,
+    python_requires=REQUIRES_PYTHON,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: OS Independent',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)

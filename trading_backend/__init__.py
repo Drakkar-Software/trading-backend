71654bec309ad7d0b3432cf452ee6a7f8aaabf47
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-08-07 23:53:22 +0200

    [Version] v1.0.14

--- trading_backend/__init__.py
+++ trading_backend/__init__.py
@@ -13,7 +13,7 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
-VERSION = "1.0.13"
+VERSION = "1.0.14"
 PROJECT_NAME = "trading-backend"
 
 from trading_backend import exchange_factory

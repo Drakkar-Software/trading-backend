a26a2ecaff5b420f2b08aa2672fc5c454953678a
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2023-06-24 14:51:39 +0200

    [Version] v1.2.4

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-VERSION = "1.2.3"
+VERSION = "1.2.4"
 PROJECT_NAME = "trading-backend"
 
 PACKAGES = find_packages(exclude=["tests"])

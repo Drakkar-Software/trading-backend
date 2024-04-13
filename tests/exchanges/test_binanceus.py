fa361b3a59700e7841e5b3e65cb09eff925dc981
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2024-04-13 17:19:35 +0200

    [BinanceUS] fix api key rights checks

--- tests/exchanges/test_binanceus.py
+++ tests/exchanges/test_binanceus.py
@@ -36,28 +36,3 @@ async def test_get_orders_parameters(binanceus_exchange):
 async def test_inner_is_valid_account(binanceus_exchange):
     exchange = exchanges.BinanceUS(binanceus_exchange)
     assert await exchange._inner_is_valid_account() == (False, await exchange._ensure_broker_status())
-
-
-@pytest.mark.asyncio
-async def test_get_api_key_rights(binanceus_exchange):
-    exchange = exchanges.BinanceUS(binanceus_exchange)
-    with mock.patch.object(
-        exchange._exchange.connector.client, "sapi_get_account_apirestrictions",
-        mock.AsyncMock(return_value={"enableReading": True, "enableSpotAndMarginTrading": False, "enableWithdrawals": False})
-    ) as sapi_get_account_apirestrictions_mock:
-        assert await exchange._get_api_key_rights() == [
-            trading_backend.enums.APIKeyRights.READING
-        ]
-        sapi_get_account_apirestrictions_mock.assert_awaited_once()
-    with mock.patch.object(
-        exchange._exchange.connector.client, "sapi_get_account_apirestrictions",
-        mock.AsyncMock(return_value={"enableReading": True, "enableSpotAndMarginTrading": True, "enableWithdrawals": True})
-    ) as sapi_get_account_apirestrictions_mock:
-        assert await exchange._get_api_key_rights() == [
-            trading_backend.enums.APIKeyRights.READING,
-            trading_backend.enums.APIKeyRights.SPOT_TRADING,
-            trading_backend.enums.APIKeyRights.MARGIN_TRADING,
-            trading_backend.enums.APIKeyRights.FUTURES_TRADING,
-            trading_backend.enums.APIKeyRights.WITHDRAWALS
-        ]
-        sapi_get_account_apirestrictions_mock.assert_awaited_once()

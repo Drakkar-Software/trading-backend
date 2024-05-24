367d66bf9c906bdbb078811918b344c32ff36cbe
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2024-05-24 09:43:41 +0200

    [Version] v1.2.22

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-VERSION = "1.2.21"
+VERSION = "1.2.22"
 PROJECT_NAME = "trading-backend"
 
 PACKAGES = find_packages(exclude=["tests"])
